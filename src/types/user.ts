28b9cbdd5a904da2d5f9e129105c998f55f58686
유승완 <51105841+Seung-wan@users.noreply.github.com>
2022-06-23 00:57:51 +0900

    add user type

---
+++ src/types/user.ts
@@ -0,0 +1,25 @@
+export interface IUser {
+  age_range: string;
+  common_bad_question02_count: number;
+  common_bad_question03_count: number;
+  common_bad_question04_count: number;
+  common_bad_question01_count: number;
+  common_bad_question05_count: number;
+  common_good_question01_count: number;
+  common_good_question02_count: number;
+  common_good_question03_count: number;
+  common_good_question04_count: number;
+  common_good_question05_count: number;
+  create_date: string;
+  dining_score: number;
+  email: string;
+  gender: string;
+  host_bad_question01_count: number;
+  host_bad_question02_count: number;
+  host_good_question01_count: number;
+  host_good_question02_count: number;
+  nickname: string;
+  profile_description: string;
+  profile_image: string;
+  use_policy_agree: string;
+}
